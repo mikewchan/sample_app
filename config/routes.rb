d9703ac48b630d6e58eb324ef21ed784ff9231c7
mikewchan <mike@mikewchan.com>
2013-12-02 10:05:45 -0500

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 SampleApp::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
-  get "index/index"
+  get "static_pages/about"
+  
+  #get "index/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root to: 'index#index'
+  #root to: 'index#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
